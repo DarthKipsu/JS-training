9284caed4d32b9369f68ec2b7155ac623d2372bc
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-04 10:26:02 +0300

    add cityService service

--- 03-angularjs/app.js
+++ 03-angularjs/app.js
@@ -12,9 +12,25 @@ app.config(function($routeProvider) {
             controller: 'forecastController'})
 });
 
+// SERVICES
+
+// This service allows me to share key data (city) between my controllers.
+app.service('cityService', function() {
+    this.city = 'Helsinki';
+});
+
 // CONTROLLERS
-app.controller('homeController', ['$scope', function($scope) {
+app.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
+
+
 }]);
 
-app.controller('forecastController', ['$scope', function($scope) {
+app.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+    $scope.city = cityService.city;
 }]);
